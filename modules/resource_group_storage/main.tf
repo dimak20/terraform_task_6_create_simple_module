122d1c204cd642a4e0632ccf818e3e5e903f85ff
Dmytro Kolchak <dima.kolhac@gmail.com>
2026-03-12 02:39:03 +0200

    5

--- modules/resource_group_storage/main.tf
+++ modules/resource_group_storage/main.tf
@@ -1,12 +1,3 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "3.105.0"
-    }
-  }
-}
-
 resource "azurerm_resource_group" "storage_group" {
   name     = var.resource_group_name
   location = var.resource_group_location
