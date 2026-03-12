c03dd6151db04c8095901e5e6247956e49c58bf1
Dmytro Kolchak <dima.kolhac@gmail.com>
2026-03-12 02:17:39 +0200

    1

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-module "mytaskmodule" {
+module "resource_group_storage" {
   source = "dimak20/terraform-azurerm-resource_group_storage"
   version = "1.0.0"
   resource_group_name = "mytaskmodule-rg"
