1153e7f9c780c016a7aac5c3074ebc6241b4a6cd
Dmytro Kolchak <dima.kolhac@gmail.com>
2026-03-12 02:36:22 +0200

    4

--- modules/resource_group_storage/main.tf
+++ modules/resource_group_storage/main.tf
@@ -7,10 +7,6 @@ terraform {
   }
 }
 
-provider "azurerm" {
-  features {}
-}
-
 resource "azurerm_resource_group" "storage_group" {
   name     = var.resource_group_name
   location = var.resource_group_location
